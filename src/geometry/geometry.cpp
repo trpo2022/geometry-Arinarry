fdc4228fd8d069e3093feb6b538676bf10edcc90
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 21:00:24 +0700

    Update geometry.cpp

--- src/geometry/geometry.cpp
+++ src/geometry/geometry.cpp
@@ -8,11 +8,11 @@ int main()
     float x_cir, y_cir, x1_tr, x2_tr, x3_tr, y1_tr, y2_tr, y3_tr, r;
 	  cout << "Enter the coordinates of the circle: ";
     cin >> x_cir >> y_cir >> r;
-  	cout << "\nEnter the coordinates x1, y1 for the triangle: ");
+  	cout << "\nEnter the coordinates x1, y1 for the triangle: ";
 	  cin >> x1_tr >> y1_tr; 
-	  cout << "\nEnter the coordinates x2, y2 for the triangle: ");
+	  cout << "\nEnter the coordinates x2, y2 for the triangle: ";
   	cin >> x2_tr >> y2_tr;
-  	cout << "\nEnter the coordinates x3, y3 for the triangle: ");
+  	cout << "\nEnter the coordinates x3, y3 for the triangle: ";
   	cin >> x3_tr >> y3_tr;
   	if (r > 0) {
         check(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
