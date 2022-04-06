9633c93649ce0c443604561de5895d5ec73e0c16
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 21:05:06 +0700

    Update circle_perimeter.cpp

--- src/libgeo/circle_perimeter.cpp
+++ src/libgeo/circle_perimeter.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void circle_perimeter(float x_cir float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr)
+int circle_perimeter(float r)
 {
 	    float Pi = 3.14, P;
 	    P = 2 * r * Pi;
