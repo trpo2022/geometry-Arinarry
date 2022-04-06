06bf14ac8f707903db24c34d784ffdcc50c41357
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 18:32:59 +0700

    Create Check.h

---
+++ src/libgeo/Check.h
@@ -0,0 +1,10 @@
+#ifndef function
+#define function
+
+int check(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+int square_cir(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+int per_cir(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+int square_tr(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+int per_trg(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+
+#endif
