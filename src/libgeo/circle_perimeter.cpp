352865edd59913a9525b9e69badbf9c57bdd7daf
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 19:19:19 +0700

    Update and rename circle_perimeter to circle_perimeter.cpp

--- src/libgeo/circle_perimeter.cpp
+++ src/libgeo/circle_perimeter.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void per_krug(float x_cir float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr)
+void circle_perimeter(float x_cir float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr)
 {
 	    float Pi = 3.14, P;
 	    P = 2 * r * Pi;
