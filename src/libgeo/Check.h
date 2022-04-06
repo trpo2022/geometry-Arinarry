86973af45276dfc491f9b1cb6280113018e77be0
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 19:21:48 +0700

    Update Check.h

--- src/libgeo/Check.h
+++ src/libgeo/Check.h
@@ -1,10 +1,10 @@
 #ifndef function
 #define function
 
-int check(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-int square_cir(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-int per_cir(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-int square_tr(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-int per_trg(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+void check(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+void circle_area(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+void circle_perimeter(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+void triangle_area(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+void triangle_perimeter(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
 
 #endif
