8281e62f9429df1fd1532b3ba737408e37c9b620
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 19:24:37 +0700

    Update triangle_area.cpp

--- src/libgeo/triangle_area.cpp
+++ src/libgeo/triangle_area.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void triangle_area(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr)
 {
-	    float a, b, c, S, P;
+	float a, b, c, S, P;
     	a = sqrt(((x2_tr - x1_tr) * (x2_tr - x1_tr) + (y2_tr - y1_tr) * (y2_tr - y1_tr)));
     	b = sqrt(((x3_tr - x2_tr) * (x3_tr - x2_tr) + (y3_tr - y2_tr) * (y3_tr - y2_tr)));
     	c = sqrt(((x1_tr - x3_tr) * (x1_tr - x3_tr) + (y1_tr - y3_tr) * (y1_tr - y3_tr)));
