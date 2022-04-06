0e5b0666ec4aad899084d3098b62e81282dde35c
Arinarry <98798726+Arinarry@users.noreply.github.com>
2022-04-06 21:03:22 +0700

    Update circle_area.cpp

--- src/libgeo/circle_area.cpp
+++ src/libgeo/circle_area.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void circle_area(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr)
+int circle_area(float r)
 {
       float Pi = 3.14, S;
 	    S = r*r*Pi;
